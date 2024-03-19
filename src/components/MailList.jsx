2103b471e17a8891fbf1bdc2a154935f95b3eb9f
LouisPino <ljpino95@gmail.com>
2024-03-19 14:19:45 -0400

    adjust mail

--- src/components/MailList.jsx
+++ src/components/MailList.jsx
@@ -1,13 +1,30 @@
+
+
 export default function Press() {
     return (
+        <div id="mc_embed_shell">
 
-        <form action="https://gmail.us18.list-manage.com/subscribe/post?u=d9502485d6c1b635fd14c4912&amp;id=8590cfffd6&amp;f_id=00c610e7f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_self" novalidate="">
-            <div class="mc-field-group"><label for="mce-EMAIL">Email Address <span class="asterisk">*</span></label><input type="email" name="EMAIL" class="required email" id="mce-EMAIL" required="" /></div>
-            <div id="mce-responses" class="clear foot">
-                <div class="response" id="mce-error-response" ></div>
-                <div class="response" id="mce-success-response" ></div>
+            <div id="mc_embed_signup">
+                <form action="https://gmail.us18.list-manage.com/subscribe/post?u=d9502485d6c1b635fd14c4912&amp;id=8590cfffd6&amp;f_id=00c610e7f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_self" novalidate="">
+                    <div id="mc_embed_signup_scroll"><h2>Subscribe</h2>
+                        <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+                        <div class="mc-field-group"><label for="mce-EMAIL">Email Address <span class="asterisk">*</span></label><input type="email" name="EMAIL" class="required email" id="mce-EMAIL" required="" /><span id="mce-EMAIL-HELPERTEXT" class="helper_text"></span></div>
+                        <div id="mce-responses" class="clear foot">
+                            <div class="response" id="mce-error-response" style={{ display: "none" }}></div>
+                            <div class="response" id="mce-success-response" style={{ display: "none" }}></div>
+                        </div>
+                        <div aria-hidden="true" style={{ position: "absolute", left: "-5000px" }}>
+                            <input type="text" name="b_d9502485d6c1b635fd14c4912_8590cfffd6" tabindex="-1" value="" />
+                        </div>
+                        <div class="optionalParent">
+                            <div class="clear foot">
+                                <input type="submit" name="subscribe" id="mc-embedded-subscribe" class="button" value="Subscribe" />
+                            </div>
+                        </div>
+                    </div>
+                </form>
             </div>
-        </form>
+        </div>
 
     )
 }
