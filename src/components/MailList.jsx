4376c2cfc97adbc2e7a4244aced71d3001ff635e
LouisPino <ljpino95@gmail.com>
2024-03-20 20:59:55 -0400

    contact and color

--- src/components/MailList.jsx
+++ src/components/MailList.jsx
@@ -3,12 +3,15 @@
 export default function Press() {
     return (
         <div id="mc_embed_shell">
-
             <div id="mc_embed_signup">
                 <form action="https://gmail.us18.list-manage.com/subscribe/post?u=d9502485d6c1b635fd14c4912&amp;id=8590cfffd6&amp;f_id=00c610e7f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_self" novalidate="">
-                    <div id="mc_embed_signup_scroll"><h2>Subscribe</h2>
-                        <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
-                        <div class="mc-field-group"><label for="mce-EMAIL">Email Address <span class="asterisk">*</span></label><input type="email" name="EMAIL" class="required email" id="mce-EMAIL" required="" /><span id="mce-EMAIL-HELPERTEXT" class="helper_text"></span></div>
+                    <div id="mc_embed_signup_scroll">
+                        <div class="indicates-required">Subscribe to our monthly newsletter!</div>
+                        <div class="mc-field-group">
+                            {/* <label for="mce-EMAIL">Email Address <span class="asterisk">*</span></label> */}
+                            <input type="email" name="EMAIL" class="required email" id="mce-EMAIL" required="" placeholder="Your Email Address" />
+                            <span id="mce-EMAIL-HELPERTEXT" class="helper_text"></span>
+                        </div>
                         <div id="mce-responses" class="clear foot">
                             <div class="response" id="mce-error-response" style={{ display: "none" }}></div>
                             <div class="response" id="mce-success-response" style={{ display: "none" }}></div>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -17,7 +17,6 @@ function Contact() {
     }, [])
     return (<>
         <ContactForm />
-        <MailList />
     </>
     )
 }
