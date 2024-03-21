d6e8272d3c739e10087dc2f1f0aa7f87da16803f
LouisPino <ljpino95@gmail.com>
2024-03-21 13:22:09 -0400

    work on carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -18,15 +18,16 @@ const Carousel = () => {
 
     return (
         <div className="carousel">
-            <a class="carousel-item" href="#one!"><img src="assets/carousel1.jpg" alt="img1" /></a>
-            <a class="carousel-item" href="#one!"><img src="assets/carousel2.jpg" alt="img1" /></a>
-            <a class="carousel-item" href="#one!"><img src="assets/carousel3.jpg" alt="img1" /></a>
-            <a class="carousel-item" href="#one!"><img src="assets/carousel4.jpg" alt="img1" /></a>
-            <a class="carousel-item" href="#one!"><img src="assets/carousel5.jpg" alt="img1" /></a>
-            <a class="carousel-item" href="#one!"><img src="assets/carousel6.jpg" alt="img1" /></a>
-            <a class="carousel-item" href="#one!"><img src="assets/carousel7.jpg" alt="img1" /></a>
-            <a class="carousel-item" href="#one!"><img src="assets/carousel8.jpg" alt="img1" /></a>
+            <a class="carousel-item"><img src="assets/carousel2.jpg" alt="img1" /></a>
+            <a class="carousel-item"><img src="assets/carousel3.jpg" alt="img1" /></a>
+            <a class="carousel-item"><img src="assets/carousel4.jpg" alt="img1" /></a>
+            <a class="carousel-item"><img src="assets/carousel5.jpg" alt="img1" /></a>
+            <a class="carousel-item"><img src="assets/carousel6.jpg" alt="img1" /></a>
+            <a class="carousel-item"><img src="assets/carousel7.jpg" alt="img1" /></a>
+            <a class="carousel-item"><img src="assets/carousel8.jpg" alt="img1" /></a>
         </div>
+
+
     );
 };
 
