912af2bf97033573ab8a0587a58a19a67897d98d
LouisPino <ljpino95@gmail.com>
2024-03-21 16:21:36 -0400

    press layout

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -3,46 +3,61 @@ import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css';
 
 const Carousel = () => {
-    const [carouselInstance, setCarouselInstance] = useState(false);
+    const [carouselIdx, setCarouselIdx] = useState(0);
     const carouselTimer = useRef(null); // Use useRef for persistent mutable storage
+    const carouselInstance = useRef(null); // Mutable reference for instances
+
 
     useEffect(() => {
-        const options = { dist: 0, indicators: true, numVisible: 5 };
-        let instances = M.Carousel.init(document.querySelector(".carousel"), options);
+        function setIdx() {
+            if (carouselInstance.current) {
+                console.log(carouselInstance.current.center);
+            }
+        }
+        const options = {
+            dist: 0,
+            indicators: true,
+            numVisible: 5,
+            onCycleTo: setIdx
+        };
+        carouselInstance.current = M.Carousel.init(document.querySelector(".carousel"), options);
+
         // Set the interval to advance the carousel
         clearInterval(carouselTimer.current);
         carouselTimer.current = setInterval(() => {
-            instances.next();
+            carouselInstance.current.next();
         }, 5000);
-        setCarouselInstance(true);
 
         // Setup event listener for window resize
         const handleResize = () => {
-            if (instances) {
-                console.log("hit")
+            if (carouselInstance.current) {
                 clearInterval(carouselTimer.current);
-                instances.destroy()
-                instances = M.Carousel.init(document.querySelector(".carousel"), options);
+                carouselInstance.current.destroy();
+                carouselInstance.current = M.Carousel.init(document.querySelector(".carousel"), options);
+                carouselInstance.current.set(carouselIdx);
                 carouselTimer.current = setInterval(() => {
-                    instances.next();
+                    carouselInstance.current.next();
                 }, 5000);
             }
         };
 
         window.addEventListener('resize', handleResize);
 
-        // Cleanup function to clear interval and remove event listener
+        // Don't forget to clean up
         return () => {
-            clearInterval(carouselTimer.current);
+            if (carouselInstance.current) {
+                clearInterval(carouselTimer.current);
+                carouselInstance.current.destroy();
+            }
             window.removeEventListener('resize', handleResize);
         };
-    }, []); // Empty array ensures this effect runs only once on mount
+    }, [carouselIdx]); // Ensure carouselIdx is captured correctly
+
     return (
         <div className="carousel">
             <a class="carousel-item"><img src="https://res.cloudinary.com/dsvcyich1/image/upload/c_fill,w_2000,h_1125,ar_16:9/v1711046650/cichorium/carousel/carousel3_u9jjjo.jpg" alt="img1" /></a>
             <a class="carousel-item"><img src="https://res.cloudinary.com/dsvcyich1/image/upload/c_fill,w_2000,h_1125,ar_16:9/v1711046652/cichorium/carousel/carousel7_veufl2.jpg" alt="img1" /></a>
             <a class="carousel-item"><img src="https://res.cloudinary.com/dsvcyich1/image/upload/c_fill,w_2000,h_1125,ar_16:9/v1711046648/cichorium/carousel/carousel5_lkpxki.jpg" alt="img1" /></a>
-            <a class="carousel-item"><img src="https://res.cloudinary.com/dsvcyich1/image/upload/c_fill,w_2000,h_1125,ar_16:9/v1711046496/cichorium/carousel/carousel1_lrvy2z.jpg" alt="img1" /></a>
             <a class="carousel-item"><img src="https://res.cloudinary.com/dsvcyich1/image/upload/c_fill,w_2000,h_1125,ar_16:9/v1711046170/cichorium/carousel/carousel4_zv4zqs.jpg" alt="img1" /></a>
             <a class="carousel-item"><img src="https://res.cloudinary.com/dsvcyich1/image/upload/c_fill,w_2000,h_1125,ar_16:9/v1711046165/cichorium/carousel/carousel6_wn4rrn.jpg" alt="img1" /></a>
             <a class="carousel-item"><img src="https://res.cloudinary.com/dsvcyich1/image/upload/c_fill,w_2000,h_1125,ar_16:9/v1711046166/cichorium/carousel/carousel8_snxrpz.jpg" alt="img1" /></a>
--- src/components/Quotes.jsx
+++ src/components/Quotes.jsx
@@ -17,10 +17,24 @@ const Quotes = () => {
 
 
     if (about) {
-        const quotes = about.quotes.map((quote) => {
-            return <h5 className="quote">{quote}</h5>
-        })
+        const quotes = about.quotes.map((quote, index) => {
+            // Split the quote into parts based on newline characters
+            const quoteParts = quote.split('\n').map((part, partIndex) => {
+                // Determine if this part is the second part
+                const isSecondPart = partIndex === 1;
+                // Apply a different class if this is the second part
+                const partClassName = isSecondPart ? "author" : "";
 
+                return (
+                    <span key={partIndex} className={partClassName}>
+                        {part}
+                        {partIndex < quote.split('\n').length - 1 && <br />}
+                    </span>
+                );
+            });
+
+            return <h5 className="quote" key={index}>{quoteParts}</h5>;
+        });
 
         return (
             <div className="quotes-ctr">
