a346a772381a5869e4ca4ac2dcdd8e49810f0a6b
LouisPino <ljpino95@gmail.com>
2024-03-22 10:01:33 -0400

    remove education

--- src/components/HomeHero.jsx
+++ src/components/HomeHero.jsx
@@ -22,7 +22,7 @@ function HomeHero() {
     const loaded = () => (
         <section className="hero">
             <Carousel />
-            <p className="bio"><span className="here bio" style={{ padding: 0 }}>Duo Cichorium </span>is comprised of interdisciplinary artists Jasmine Tsui and Louis Pino. Like the Chicory plant, the two performers are not native to Ontario, but have taken root in the environment of Toronto following their tertiary education.</p>
+            <p className="bio"><span className="here bio" style={{ padding: 0 }}>Duo Cichorium </span>is comprised of interdisciplinary artists Jasmine Tsui and Louis Pino. Like the Chicory plant, the two performers are not native to Ontario, but have taken root in the environment of Toronto.</p>
             <Link className="Link" to="/about"><button className="button margin-auto center-text">Learn More</button></Link>
         </section >
     );
