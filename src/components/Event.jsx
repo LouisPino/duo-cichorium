4c05eeaff85bb12b25c972453394141b8efed508
LouisPino <ljpino95@gmail.com>
2024-03-22 18:20:10 -0400

    events fix

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -11,11 +11,11 @@ const Event = ({ event }) => {
                 <h5 className="event-card-title">{event.title}</h5>
                 <Photo photo={event.photo} />
                 <h5 className="event-card-date">{event.date}</h5>
-                <h5 className="event-card-date">{event.location}</h5>
+                <h5 className="event-card-location">{event.location}</h5>
             </div>
             <div className="event-card-r">
-                {event.organizer ? <h5 className="event-card-title">Presented by {event.organizer}</h5> : ""}
-                {event.series ? <p className="italic">{event.series} Series</p> : ""}
+                {event.organizer ? <h5 className="event-card-presenter">Presented by {event.organizer}</h5> : ""}
+                {event.series ? <p className="italic event-card-series">{event.series} Series</p> : ""}
                 <p className="event-card-desc">{event.shortDesc}</p>
             </div>
             <p className="mobile-only click-for-more">- Tap to learn more -</p>
