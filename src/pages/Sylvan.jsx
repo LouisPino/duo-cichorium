19ca27e556054ba082095eb939a2eebc8925211f
LouisPino <ljpino95@gmail.com>
2024-03-24 09:31:54 -0400

    bio

--- src/pages/Sylvan.jsx
+++ src/pages/Sylvan.jsx
@@ -3,7 +3,7 @@
 export default function Sylvan() {
     return (
         <>
-            <h4 className="center-text">Stay tuned for updates on our events audience interactive Choose-Your-Own-Adventure piece!</h4>
+            <h4 className="center-text">Stay tuned for updates on our upcoming audience interactive Choose-Your-Own-Adventure piece!</h4>
         </>
 
 
