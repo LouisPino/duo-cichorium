0a0287d65bfefeeaa65d711eac388508115b8b36
LouisPino <ljpino95@gmail.com>
2024-03-25 11:00:00 -0400

    header lineup

--- src/pages/Tranzac.jsx
+++ src/pages/Tranzac.jsx
@@ -1,10 +1,21 @@
-
+import "../styles/tranzac.css"
+import Photo from "../components/Photo.jsx"
 
 export default function Tranzac() {
+    const photo = {
+        "url": "https://res.cloudinary.com/dsvcyich1/image/upload/v1711046496/cichorium/carousel/carousel1_lrvy2z.jpg",
+        "credit": "ArrayMusic, Feb. 8, 2024",
+        "title": "Speed Run"
+    }
+
+
+
     return (
-        <>
-            Where we advertise and document Tranzac stuff
-        </>
+        <div className="tranzac-page">
+            <Photo photo={photo} />
+            <h4 className="tranzac-p">We will be one of 14 ensembles in residence at The TRANZAC Club from May to October 2024 </h4>
+            <h5 className="tranzac-p">Stay tuned for updates and  videos for the upcoming season!!!</h5>
+        </div>
 
 
     )
