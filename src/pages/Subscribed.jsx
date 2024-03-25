da3193ee7d555a4b85e9a82ccb568bad7e20c07c
LouisPino <ljpino95@gmail.com>
2024-03-25 17:29:53 -0400

    subscribed page

---
+++ src/pages/Subscribed.jsx
@@ -0,0 +1,12 @@
+import PageTitle from "../components/PageTitle";
+
+
+export default function Subscribed() {
+    return (
+        <>
+            <h4 className="center-text">Thank you for subscribing!</h4>
+        </>
+
+
+    )
+}
