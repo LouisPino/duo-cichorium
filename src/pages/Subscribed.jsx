733f51afda46f844bbc2a9afe7509ea1e7e7f6a8
LouisPino <ljpino95@gmail.com>
2024-03-25 17:34:44 -0400

    subscribed page

--- src/pages/Subscribed.jsx
+++ src/pages/Subscribed.jsx
@@ -3,8 +3,7 @@ import PageTitle from "../components/PageTitle";
 
 export default function Subscribed() {
     return (
-        <>
-            <h4 className="center-text">Thank you for subscribing!</h4>
+        <><PageTitle page="Thanks for subscribing!" />
         </>
 
 
