9e0a1870f2819718ae61ed5968fc2da29bee621f
LouisPino <ljpino95@gmail.com>
2024-04-04 17:07:38 -0400

    fix materialbox

--- src/components/MailList.jsx
+++ src/components/MailList.jsx
@@ -4,12 +4,12 @@ export default function MailList() {
     return (
         <div id="mc_embed_shell">
             <div id="mc_embed_signup" className="contact-form-ctr mail-form-ctr">
-                <form action="https://gmail.us18.list-manage.com/subscribe/post?u=d9502485d6c1b635fd14c4912&amp;id=8590cfffd6&amp;f_id=00c610e7f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_self" novalidate="">
+                <form action="https://gmail.us18.list-manage.com/subscribe/post?u=d9502485d6c1b635fd14c4912&amp;id=8590cfffd6&amp;f_id=00c610e7f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_self" novalidate="">
                     <div id="mc_embed_signup_scroll">
-                        <div class="indicates-required subscribe-text">Subscribe to our monthly newsletter!</div>
-                        <div class="mc-field-group">
-                            <input type="email" name="EMAIL" class="required email" id="mce-EMAIL" required="" placeholder="Your Email Address" />
-                            <button type="submit" name="subscribe" id="mc-embedded-subscribe" class="button" value="Subscribe" >Subscribe</button>
+                        <div className="indicates-required subscribe-text">Subscribe to our monthly newsletter!</div>
+                        <div className="mc-field-group">
+                            <input type="email" name="EMAIL" className="required email" id="mce-EMAIL" required="" placeholder="Your Email Address" />
+                            <button type="submit" name="subscribe" id="mc-embedded-subscribe" className="button" value="Subscribe" >Subscribe</button>
                         </div>
                         <div aria-hidden="true" style={{ position: "absolute", left: "-5000px" }}>
                             <input type="text" name="b_d9502485d6c1b635fd14c4912_8590cfffd6" tabindex="-1" value="" />
--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -1,8 +1,14 @@
 import { useLocation } from 'react-router-dom';
+import M from "materialize-css";
+import {useEffect } from "react"
+
 
 export default function Photo({ photo, carousel }) {
     const location = useLocation()
-
+    useEffect(() => {
+        var elems = document.querySelectorAll(".materialboxed");
+        M.Materialbox.init(elems);
+      }, []);
     return (
         <div className={carousel ? "img-ctr carousel-item" : "img-ctr"}>
             <img src={photo.url} className={carousel ? "carousel-photo" : `press-photo ${location.pathname === "/press" ? "materialboxed" : ""}`} alt={photo.imgAlt} />
