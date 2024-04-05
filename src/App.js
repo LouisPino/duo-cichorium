733f3d689b736fc2061d3d4e073666c8104b67a2
LouisPino <ljpino95@gmail.com>
2024-04-05 11:01:29 -0400

    sync

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
   return (
     <div className="App">
       <Header burgerDrop={burgerDrop} setBurgerDrop={setBurgerDrop} workDrop={workDrop} setWorkDrop={setWorkDrop} />
-      <FlowerKaboom x="1vw" y="100px" flower="0" size={150} instance={0}/>
+      <FlowerKaboom x="2vw" y="100px" flower="0" size={150} instance={0}/>
       <FlowerKaboom x="calc(95vw - 100px)" y="70vh" flower="1" size={125} instance={1}/>
       <FlowerKaboom x="calc(90vw - 100px)" y="80vh" flower="2" size={100} instance={2}/>
       <Routes>
