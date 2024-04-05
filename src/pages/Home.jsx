75cf0566e2c9ea6b7e9e920041b61a9c540c8cf1
LouisPino <ljpino95@gmail.com>
2024-04-05 10:54:19 -0400

    flower positions

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,9 +7,9 @@ import "../styles/home.css"
 export default function Home({ burgerDrop, setBurgerDrop, workDrop, setWorkDrop }) {
     return (
         <>
-            <FlowerKaboom x="100px" y="300px" flower="0" size={50} instance={0}/>
-            <FlowerKaboom x="100px" y="100px" flower="1" size={100} instance={1}/>
-            <FlowerKaboom x="100px" y="200px" flower="2" size={75} instance={2}/>
+            <FlowerKaboom x="1vw" y="100px" flower="0" size={150} instance={0}/>
+            <FlowerKaboom x="calc(95vw - 100px)" y="70vh" flower="1" size={125} instance={1}/>
+            <FlowerKaboom x="calc(90vw - 100px)" y="80vh" flower="2" size={100} instance={2}/>
             <HomeHero burgerDrop={burgerDrop} setBurgerDrop={setBurgerDrop} workDrop={workDrop} setWorkDrop={setWorkDrop} />
             <hr />
             <RecentProjects category="Featured Work" />
