3436bcd7746b683654d402d4512b38949981581f
LouisPino <ljpino95@gmail.com>
2024-04-07 19:12:47 -0400

    404

--- src/pages/_404.jsx
+++ src/pages/_404.jsx
@@ -1,7 +1,12 @@
-import PageTitle from "../components/PageTitle.jsx"
+import Photo from "../components/Photo"
 
 export default function _404() {
-       return (<>
-        <PageTitle page="404! THIS PAGE AIN'T IT DUDE!" />
-    </>)
+       return (<div className="_404">
+        <Photo photo={       {
+      "url": "https://res.cloudinary.com/dsvcyich1/image/upload/v1712531266/Tilly404_c2ootw.jpg",
+      "credit": "",
+      "title": "",
+      "imgAlt": "Picture of a cat playing with chichory flowers"
+    }}/>
+    </div>)
 }
