ef54be8185cfe91d3cb34c46d4636336b59f645f
LouisPino <ljpino95@gmail.com>
2024-04-07 19:34:42 -0400

    social flowers

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,10 +8,12 @@ function Footer() {
       <div className="footer-content">
         <div className="socials-ctr">
           <a href="https://www.instagram.com/duo_cichorium/" target="_blank" rel="noreferrer">
-            <img src="https://res.cloudinary.com/dsvcyich1/image/upload/v1693442901/instagram-logo_aoujxu.png" alt="Duo Cichorium Instagram link" />
+            <img src="https://res.cloudinary.com/dsvcyich1/image/upload/v1712532048/Insta_iwv0wl.png" alt="Duo Cichorium Instagram link" />
+            <img className="social-flower social-l" src="https://res.cloudinary.com/dsvcyich1/image/upload/v1712532187/YouTube_hover_gif_jv2llb.gif" alt="Duo Cichorium Background Flower" />
           </a>
           <a href="https://www.youtube.com/@DuoCichorium" target="_blank" rel="noreferrer" >
-            <img src="https://res.cloudinary.com/dsvcyich1/image/upload/v1711065183/paper-black-and-white-logo-pattern-youtube-play-button-6f7fda0489cbea1d1b4a036e5a111e85_pqdb7a.png" alt="Duo Cichorium YouTube link" />
+            <img className="social-flower social-r" src="https://res.cloudinary.com/dsvcyich1/image/upload/v1712532187/YouTube_hover_gif_jv2llb.gif" alt="Duo Cichorium Background Flower" />
+            <img src="https://res.cloudinary.com/dsvcyich1/image/upload/v1712532050/YouTube_vq9fpt.png" alt="Duo Cichorium YouTube link" />
           </a>
         </div>
         <h6 className="glow myh3 footer-text">© Louis Pino & Jaz Tsui 2024</h6>
