4cbbe0d0cb30939d9640fc230e7b92dc2d9051de
LouisPino <ljpino95@gmail.com>
2024-04-11 16:43:44 -0400

    soup

--- src/components/TranzacEvent.jsx
+++ src/components/TranzacEvent.jsx
@@ -5,17 +5,25 @@ import "../styles/events.css"
 
 const Event = ({ event }) => {
     return (
-        <Link className="event-card" to={`/${event.url}`} >
-            <div className="event-card-l">
+        <Link className="t-event-card" to={`/${event.url}`} >
+            <div className="t-event-card-top">
+                <h5 className="event-card-title glow">{event.title}</h5>
+            </div>
+
+
+            <div className="t-event-card-middle">
+                <div className="t-event-card-l">
                 <Photo photo={event.photo} />
+                </div>
+                <h4 className="">{event.guests}</h4>
+                {/* <h5 className="">PEOPLE SOUP {event.number}</h5> */}
+                <p className="event-card-desc">{event.shortDesc}</p>
+            </div>
+
+
+            <div className="t-event-card-bottom">
                 <h5 className="event-card-date">{event.date}</h5>
                 <h5 className="event-card-location">{event.location}</h5>
-            </div>
-            <div className="event-card-r">
-                <h5 className="">PEOPLE SOUP {event.number}</h5>
-                <h5 className="event-card-title italic">{event.title}</h5>
-                <h4 className="">{event.guests}</h4>
-                <p className="event-card-desc">{event.shortDesc}</p>
             </div>
             <p className="mobile-only click-for-more">- Tap to learn more -</p>
         </Link>
