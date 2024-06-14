612f10ead48fd7c1ef8b4dcd896f2dcadd879a6d
LouisPino <ljpino95@gmail.com>
2024-06-14 11:03:33 -0400

    add sylvan posts

--- src/pages/Sylvan.jsx
+++ src/pages/Sylvan.jsx
@@ -6,7 +6,9 @@ export default function Sylvan() {
     return (
         <>
             <PageTitle page="The Sylvan Legacy" />
-            <h4 className="center-text">Stay tuned for updates on our upcoming audience interactive Choose-Your-Own-Adventure piece!</h4>
+            <h4 className="center-text">Stay tuned for updates on our upcoming audience interactive Choose-Your-Own-Adventure multimedia piece!</h4>
+            <h5 className="center-text">Software development for audience interaction is underway, while we are in the drafting stages of composition, character, and setting design. Stay tuned for sample materials in the coming weeks!</h5>
+            <h5 className="center-text">We are delighted to be joined by lighting designer <a className="glow" href="http://www.billywong.asia/" target="_blank">Billy Wong</a> and spatial sound designer <a className="glow" href="https://www.instagram.com/fish_sh_0111/" target="_blank">Fish Yu</a> for this performance!</h5>
             <h5 className="center-text">Many thanks to the Toronto Arts Council for funding this project!!!</h5>
 
         </>
