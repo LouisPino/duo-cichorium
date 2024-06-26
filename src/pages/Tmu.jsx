ccaef0c91901c8521affaa7b75523b3361ae8f0c
LouisPino <ljpino95@gmail.com>
2024-06-26 10:42:48 -0400

    tmu moon

--- src/pages/Tmu.jsx
+++ src/pages/Tmu.jsx
@@ -1,13 +1,24 @@
 import PageTitle from "../components/PageTitle";
 import Photo from "../components/Photo";
+import "../styles/tmu.css"
 
 
 export default function Tmu() {
     return (
         <>
             <PageTitle page="AI Tools for Artists" />
-            <h4 className="center-text">We will be leading an exploratory course at Toronto Metropolitan University meant to increase artists' accesibility to non-generative AI tools for art making. Stay tuned for updates and resources!</h4>
-            <h5 className="center-text">Many thanks to the Toronto Arts Council for funding this project through the Digital Solutions Incubator program!</h5>
+            <div className="tmu-main">
+                <h4 className="center-text">We will be leading an exploratory course at Toronto Metropolitan University meant to increase artists' accesibility to non-generative AI tools for art making.</h4>
+                <div className="moon">
+                    <h1>Computer Vision Moon Resizing and Sorting</h1>
+                    <div className="iframe-ctr">
+                        <iframe src="https://www.youtube.com/embed/XJ0QcLnkgIw"></iframe>
+                    </div>
+                    <h5>Over many months, Jasmine has been taking photos of the moon every night. In order to crop and resize these photos to a standard size, we are using computer vision via OpenCV's Python library to find the moon in each image and crop a square around it. We are then using computer vision to determine how full the moon is, and then sort the images by fullness. Computer vision allows us to process hundreds of photos at once, which would otherwise take hours upon hours to do manually. The max patch above is used to quickly jump through and display each image in order.</h5>
+                </div>
+                <h4 className="center-text">Many thanks to the Toronto Arts Council for funding this project through the Digital Solutions Incubator program!</h4>
+                <img src="https://res.cloudinary.com/dsvcyich1/image/upload/v1719412705/images_y6yzjq.png" width="200px" />
+            </div>
         </>
 
 
