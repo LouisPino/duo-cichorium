45856aba27ec707a03d320c1f3ccabe630a8cf1b
LouisPino <ljpino95@gmail.com>
2024-06-28 17:33:13 -0400

    sylvan tac logo

--- src/pages/Sylvan.jsx
+++ src/pages/Sylvan.jsx
@@ -10,6 +10,7 @@ export default function Sylvan() {
             <h5 className="center-text">Software development for audience interaction is underway, while we are in the drafting stages of composition, character, and setting design. Stay tuned for sample materials in the coming weeks!</h5>
             <h5 className="center-text">We are delighted to be joined by lighting designer <a className="glow" href="http://www.billywong.asia/" target="_blank">Billy Wong</a> and spatial sound designer <a className="glow" href="https://www.instagram.com/fish_sh_0111/" target="_blank">Fish Yu</a> for this performance!</h5>
             <h5 className="center-text">Many thanks to the Toronto Arts Council for funding this project!!!</h5>
+            <img src="https://res.cloudinary.com/dsvcyich1/image/upload/v1719412705/images_y6yzjq.png" width="200px" />
 
         </>
 
