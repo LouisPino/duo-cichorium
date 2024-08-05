1ab37ec5f3efc3057050022da0a22d54d93af12a
LouisPino <ljpino95@gmail.com>
2024-08-05 10:24:07 -0400

    sylvan

--- src/pages/Sylvan.jsx
+++ src/pages/Sylvan.jsx
@@ -1,5 +1,7 @@
 import PageTitle from "../components/PageTitle";
 import Photo from "../components/Photo";
+import "../styles/sylvan.css"
+
 
 
 export default function Sylvan() {
@@ -7,11 +9,17 @@ export default function Sylvan() {
         <>
             <PageTitle page="The Sylvan Legacy" />
             <h4 className="center-text">Stay tuned for updates on our upcoming audience interactive Choose-Your-Own-Adventure multimedia piece!</h4>
-            <h5 className="center-text">Software development for audience interaction is underway, while we are in the drafting stages of composition, character, and setting design. Stay tuned for sample materials in the coming weeks!</h5>
-            <h5 className="center-text">We are delighted to be joined by lighting designer <a className="glow" href="http://www.billywong.asia/" target="_blank">Billy Wong</a> and spatial sound designer <a className="glow" href="https://www.instagram.com/fish_sh_0111/" target="_blank">Fish Yu</a> for this performance!</h5>
-            <h5 className="center-text">Many thanks to the Toronto Arts Council for funding this project!!!</h5>
-            <img src="https://res.cloudinary.com/dsvcyich1/image/upload/v1719412705/images_y6yzjq.png" width="200px" />
-
+            <div className="sylvan-entry">
+                <div className=" iframe-ctr">
+                    <iframe src={"https://youtube.com/embed/VwIg59Vlm1s"} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                </div>
+                <h5 className="center-text">Getting started on animation and character design!</h5>
+            </div>
+            <h3 className="center-text sylvan-entry">We are delighted to be joined by lighting designer <a className="glow" href="http://www.billywong.asia/" target="_blank">Billy Wong</a> and spatial sound designer <a className="glow" href="https://www.instagram.com/fish_sh_0111/" target="_blank">Fish Yu</a> for this performance!</h3>
+            <div className="funders-imgs sylvan-entry">
+                <h5 className="center-text ">This project is produced with the support of the City of Toronto through Toronto Arts Council.</h5>
+                <img className="funders-img" src="https://res.cloudinary.com/dsvcyich1/image/upload/v1719412705/images_y6yzjq.png" width="200px" />
+            </div>
         </>
 
 
--- src/pages/Tmu.jsx
+++ src/pages/Tmu.jsx
@@ -16,8 +16,10 @@ export default function Tmu() {
                     </div>
                     <h5>Over many months, Jasmine has been taking photos of the moon every night. In order to crop and resize these photos to a standard size, we are using computer vision via OpenCV's Python library to find the moon in each image and crop a square around it. We are then using computer vision to determine how full the moon is, and then sort the images by fullness. Computer vision allows us to process hundreds of photos at once, which would otherwise take hours upon hours to do manually. The max patch above is used to quickly jump through and display each image in order.</h5>
                 </div>
-                <h4 className="center-text">Many thanks to the Toronto Arts Council for funding this project through the Digital Solutions Incubator program!</h4>
-                <img src="https://res.cloudinary.com/dsvcyich1/image/upload/v1719412705/images_y6yzjq.png" width="200px" />
+                <div className="funders-imgs sylvan-entry">
+                    <h5 className="center-text ">This project is produced with the support of the City of Toronto through Toronto Arts Council.</h5>
+                    <img className="funders-img" src="https://res.cloudinary.com/dsvcyich1/image/upload/v1719412705/images_y6yzjq.png" width="200px" />
+                </div>
             </div>
         </>
 
