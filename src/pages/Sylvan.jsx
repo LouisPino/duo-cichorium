cb998979becebeefbcb2c8469c3c2876f69b5fc7
LouisPino <ljpino95@gmail.com>
2024-08-15 22:19:15 -0400

    paint water poster

--- src/pages/Sylvan.jsx
+++ src/pages/Sylvan.jsx
@@ -15,7 +15,7 @@ export default function Sylvan() {
                 </div>
                 <h5 className="center-text">Getting started on animation and character design!</h5>
             </div>
-            <h3 className="center-text sylvan-entry">We are delighted to be joined by lighting designer <a className="glow" href="http://www.billywong.asia/" target="_blank">Billy Wong</a> and spatial sound designer <a className="glow" href="https://www.instagram.com/fish_sh_0111/" target="_blank">Fish Yu</a> for this performance!</h3>
+            {/* <h3 className="center-text sylvan-entry">We are delighted to be joined by lighting designer <a className="glow" href="http://www.billywong.asia/" target="_blank">Billy Wong</a> and spatial sound designer <a className="glow" href="https://www.instagram.com/fish_sh_0111/" target="_blank">Fish Yu</a> for this performance!</h3> */}
             <div className="funders-imgs sylvan-entry">
                 <h5 className="center-text ">This project is produced with the support of the City of Toronto through Toronto Arts Council.</h5>
                 <img className="funders-img" src="https://res.cloudinary.com/dsvcyich1/image/upload/v1719412705/images_y6yzjq.png" width="200px" />
