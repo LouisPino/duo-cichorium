9b3daaee88845b251447edee28a2b4c3c9b1d977
LouisPino <ljpino95@gmail.com>
2024-10-28 15:25:47 -0400

    playscape update

--- src/components/PlayscapeEvent.jsx
+++ src/components/PlayscapeEvent.jsx
@@ -20,7 +20,7 @@ const Event = ({ event }) => {
 
 
             <div className="playscape-event-card-bottom">
-                {<h4 className="">{event.guests ? event.guests : "GUEST ARTSISTS TBD"}</h4>}
+                {<h4 className="">{event.guests ? `Guests: ${event.guests}` : "GUEST ARTSISTS TBD"}</h4>}
                 <h6 className="event-card-desc">{event.desc}</h6>
                 <h5 className="event-card-date">{event.date}</h5>
                 <h5 className="event-card-location">{event.location}</h5>
