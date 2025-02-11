d097ea092cabba9110822f1d5751bc7f22a34b0c
LouisPino <ljpino95@gmail.com>
2025-02-11 17:20:40 -0500

    sylvan page

--- src/pages/Sylvan.jsx
+++ src/pages/Sylvan.jsx
@@ -15,13 +15,33 @@ export default function Sylvan() {
                 <h5 className="center-text"> Play the Sylvan Legacy Game!!!!</h5>
                 <iframe src="https://louispino.github.io/sylvan-game/" id="game-iframe"></iframe>
             </div>
+
+
+            <div className="sylvan-entry">
+                <h5 className="center-text"> Developing a system to trigger text, audio, image, and video changes with one click.</h5>
+                <div className="iframe-ctr sylvan-iframe-ctr"><iframe src="https://drive.google.com/file/d/1CBOoOQQLp0J5zq8llb3Z8Ru3yP2u6q9l/preview" frameborder="0"></iframe></div>
+            </div>
+            <div className="sylvan-entry sylvan-images-div">
+                <h5 className="center-text"> Map and environment sketches </h5>
+                <div className="sylvan-images">
+                    <img src="https://res.cloudinary.com/dsvcyich1/image/upload/v1739310614/Beach_to_Ocean_Jan17_ics5zm.png"></img>
+                    <img src="https://res.cloudinary.com/dsvcyich1/image/upload/v1739310612/Cloud_to_Space_Jan17_zk7d43.png"></img>
+                    <img src="https://res.cloudinary.com/dsvcyich1/image/upload/v1739310612/Sylvan_world_map_Jan17_ggl3mh.png"></img>
+                    <img src="https://res.cloudinary.com/dsvcyich1/image/upload/v1739310611/Cave_map_Jan17_q9pwwq.png"></img>
+
+
+                </div>
+            </div >
+
+
+
             <div className="sylvan-entry">
                 <h5 className="center-text">Check out this demonstration of some of the preliminary interactive elements of the piece!</h5>
-                <div className="iframe-ctr"><iframe src="https://www.youtube.com/embed/gYSEOw8xKlo" frameborder="0"></iframe></div>
+                <div className="iframe-ctr sylvan-iframe-ctr"><iframe src="https://www.youtube.com/embed/gYSEOw8xKlo" frameborder="0"></iframe></div>
             </div>
             <div className="sylvan-entry">
                 <h5 className="center-text">Getting started on animation and character design!</h5>
-                <div className="iframe-ctr" >
+                <div className="iframe-ctr sylvan-iframe-ctr" >
                     <iframe src={"https://youtube.com/embed/VwIg59Vlm1s"} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                 </div>
             </div>
