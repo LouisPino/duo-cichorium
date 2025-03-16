d291d1b4c9dab3ac532b263fb19d6fb031a47c09
LouisPino <ljpino95@gmail.com>
2025-03-15 20:47:50 -0400

    subscribe

--- src/components/MailList.jsx
+++ src/components/MailList.jsx
@@ -6,7 +6,7 @@ export default function MailList() {
             <div id="mc_embed_signup" className="contact-form-ctr mail-form-ctr">
                 <form action="https://gmail.us18.list-manage.com/subscribe/post?u=d9502485d6c1b635fd14c4912&amp;id=8590cfffd6&amp;f_id=00c610e7f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_self" novalidate="">
                     <div id="mc_embed_signup_scroll">
-                        <div className="indicates-required subscribe-text">Subscribe to our monthly newsletter!</div>
+                        <div className="indicates-required subscribe-text">Get emails from us!</div>
                         <div className="mc-field-group">
                             <input type="email" name="EMAIL" className="required email" id="mce-EMAIL" required="" placeholder="Your Email Address" />
                             <button type="submit" name="subscribe" id="mc-embedded-subscribe" className="button" value="Subscribe" >Subscribe</button>
